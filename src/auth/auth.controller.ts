befe0fa43a475f92fb9ebabe0507e5f76c05bfe9
Agustin Petrillo <agustinpetrillo1@gmail.com>
2023-09-13 01:04:16 -0300

    remove guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from 'src/dto/auth/signUp.dto';
 import { LoginDto } from 'src/dto/auth/login.dto';
