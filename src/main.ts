f20e2e2d5287818c2a4e3d76a2eabae2b1739a81
Agustin Petrillo <agustinpetrillo1@gmail.com>
2023-09-13 15:46:02 -0300

    added 8080

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT, () => {
+  await app.listen(process.env.PORT ?? 8080, () => {
     process.env.PORT ?? 8080, '0.0.0.0';
   });
 }
