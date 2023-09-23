04442b61998387d4bd70a3294937ddf85eb07af4
Agustin Petrillo <agustinpetrillo1@gmail.com>
2023-09-23 15:13:02 -0300

    new req.user for retriving data

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,13 +1,14 @@
 import {
   Controller,
-  Post,
   Body,
   Get,
-  Param,
-  Query,
   Delete,
   Put,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
+import { Request } from 'express';
+// import { AuthGuard } from '@nestjs/passport';
 import { AccountsService } from './accounts.service';
 import { DeleteAccountIdDto } from 'src/dto/accounts/delete.dto';
 import { PasswordDto } from 'src/dto/accounts/password.dto';
@@ -17,29 +18,26 @@ import { EmailAccountDto } from 'src/dto/accounts/email.dto';
 export class AccountsController {
   constructor(private accountService: AccountsService) {}
 
-  //api/user/profile/settings/65023287ef22e863fbd8689f
-  // @Get('/profile/settings/:id')
-  // getAccount(@Param('id') userId: string) {
-  //   return this.accountService.getAccount(userId);
-  // }
-
-  //api/user/profile/settings?userId=65023287ef22e863fbd8689f
-  @Get('/profile/settings')
-  getAccount(@Query('userId') userId: string) {
-    return this.accountService.getAccount(userId);
+  @Get('/profile/settings/me')
+  // @UseGuards(AuthGuard())
+  getAccount(@Req() req: Request) {
+    return req.user;
   }
 
   @Delete('/profile/settings/delete')
+  // @UseGuards(AuthGuard())
   deleteAccount(@Body() userId: DeleteAccountIdDto) {
     return this.accountService.deleteAccount(userId);
   }
 
   @Put('/profile/settings/modifyPassword')
+  // @UseGuards(AuthGuard())
   modifyPassword(@Body() newPassword: PasswordDto, userId: string) {
     return this.accountService.modifyPassword(newPassword, userId);
   }
 
   @Put('/profile/settings/modifyEmail')
+  // @UseGuards(AuthGuard())
   modifyEmail(@Body() newEmail: EmailAccountDto, userId: string) {
     return this.accountService.modifyEmail(newEmail, userId);
   }
