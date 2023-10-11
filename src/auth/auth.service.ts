949034514e16052f5c9db87ebde9b2c9b2850e08
Agustin Petrillo <agustinpetrillo1@gmail.com>
2023-10-10 21:32:44 -0300

    new bcrypt types

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import { User } from 'src/schemas/user.schema';
 import { SignUpDto } from 'src/dto/auth/signUp.dto';
 import { LoginDto } from 'src/dto/auth/login.dto';
